bdd48da261c49202077e75f7f3b12587027bd79c
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 05:59:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,8 +48,7 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {},
-	      :pennsylvania_railroad => {}
+	      :reading_railroad => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
